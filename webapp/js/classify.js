45284ea40008e5b54acde3ff79e8c1264fc92e84
Halfprosperous <m18286136613@163.com>
2018-02-28 20:23:00 +0800

    样式改变

--- webapp/js/classify.js
+++ webapp/js/classify.js
@@ -2,4 +2,20 @@ $(function(){
 	$(".top-img").click(function(){
 		window.location.href="../index.html";
 	});
+
+    $(".index").click(function(){
+        window.location.href="index.html";
+    });
+    $(".classify1").click(function(){
+        window.location.href="html/classify1.html";
+    });
+    $(".img3").click(function(){
+        window.location.href="html/register.html";
+    });
+    $(".food").click(function(){
+        window.location.href="html/food.html";
+    });
+    $(".myself").click(function(){
+        window.location.href="html/myself.html";
+    });
 });
---
+++ webapp/js/myself.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $(".index").click(function(){
+        window.location.href="index.html";
+    });
+    $(".classify1").click(function(){
+        window.location.href="html/classify1.html";
+    });
+    $(".img3").click(function(){
+        window.location.href="html/register.html";
+    });
+    $(".food").click(function(){
+        window.location.href="html/food.html";
+    });
+    $(".myself").click(function(){
+        window.location.href="html/myself.html";
+    });
+});
